6937740690be5632f2b654f107fa4087ec67b279
Lauren Curtis <44892963+imagineiluv@users.noreply.github.com>
2025-09-16 01:07:32 +0900

    feat: enhance organization tree mobile UX

--- src/Web/NexaCRM.WebClient/Models/OrganizationModels.cs
+++ src/Web/NexaCRM.WebClient/Models/OrganizationModels.cs
@@ -1,25 +1,15 @@
+using System.Collections.Generic;
+
 namespace NexaCRM.WebClient.Models.Organization;
 
-using System.ComponentModel.DataAnnotations;
-
-public class OrganizationUnit
-{
-    public int Id { get; set; }
-
-    [Required]
-    public string Name { get; set; } = string.Empty;
-
-    public int? ParentId { get; set; }
-}
+public record OrganizationUnit(int Id, string Name, int? ParentId);
 
 public record OrganizationStats(string UnitName, int MemberCount);
 
-public class OrganizationUser
+public class OrganizationNode
 {
-    public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Role { get; set; } = "";
-    public string Status { get; set; } = "";
+    public OrganizationUnit Unit { get; set; } = default!;
+    public List<OrganizationNode> Children { get; set; } = new();
+    public bool IsExpanded { get; set; }
 }
 
