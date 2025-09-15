8c9b346d31bc2c53fedb60d34a6287841eb03af6
Lauren Curtis <44892963+imagineiluv@users.noreply.github.com>
2025-09-16 01:07:43 +0900

    style: refine statistics dashboard mobile layout

--- src/Web/NexaCRM.WebClient/Services/StatisticsService.cs
+++ src/Web/NexaCRM.WebClient/Services/StatisticsService.cs
@@ -1,22 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using NexaCRM.WebClient.Models.Statistics;
 using NexaCRM.WebClient.Services.Interfaces;
-using System.Threading.Tasks;
 
 namespace NexaCRM.WebClient.Services;
 
 public class StatisticsService : IStatisticsService
 {
-    public Task<StatisticsSummary> GetStatisticsAsync(string? companyId = null, string? teamId = null, string? memberId = null)
+    public Task<StatisticsResult> GetStatisticsAsync(DateTime startDate, DateTime endDate)
     {
-        var members = 100;
-        var logins = 200;
-        var downloads = 50;
+        var rand = new Random();
+        var dates = Enumerable.Range(0, (endDate - startDate).Days + 1)
+            .Select(offset => startDate.AddDays(offset))
+            .ToList();
 
-        if (!string.IsNullOrEmpty(companyId)) members += 10;
-        if (!string.IsNullOrEmpty(teamId)) logins += 20;
-        if (!string.IsNullOrEmpty(memberId)) downloads += 5;
+        var loginTrend = dates
+            .Select(d => new TrendPoint(d, rand.Next(0, 100)))
+            .ToList();
 
-        return Task.FromResult(new StatisticsSummary(members, logins, downloads));
+        var downloadTrend = dates
+            .Select(d => new TrendPoint(d, rand.Next(0, 50)))
+            .ToList();
+
+        var summary = new StatisticsSummary(
+            TotalMembers: rand.Next(1000, 10000),
+            TotalLogins: loginTrend.Sum(p => p.Value),
+            TotalDownloads: downloadTrend.Sum(p => p.Value)
+        );
+
+        return Task.FromResult(new StatisticsResult(summary, loginTrend, downloadTrend));
     }
 }
 
