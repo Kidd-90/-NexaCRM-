359f855b753feb4bf031d68e4cfa8f0ec40b4f32
Lauren Curtis <44892963+imagineiluv@users.noreply.github.com>
2025-09-28 05:11:34 +0900

    fix(webserver): ensure server host serves blazor shell

--- src/NexaCRM.WebServer/Program.cs
+++ src/NexaCRM.WebServer/Program.cs
@@ -14,6 +14,7 @@ public static class Program
         Host.CreateDefaultBuilder(args)
             .ConfigureWebHostDefaults(webBuilder =>
             {
+                webBuilder.UseStaticWebAssets();
                 webBuilder.UseStartup<Startup>();
             });
 }
