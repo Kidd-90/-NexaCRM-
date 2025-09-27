167399a0a3e3ec9886d69918efc3a72a730b382c
Lauren Curtis <44892963+imagineiluv@users.noreply.github.com>
2025-09-27 13:37:22 +0900

    feat(webserver): add startup bootstrapping

--- src/NexaCRM.WebServer/Program.cs
+++ src/NexaCRM.WebServer/Program.cs
@@ -1,77 +1,19 @@
-using Microsoft.AspNetCore.Components.Authorization;
-using NexaCRM.Service.DependencyInjection;
-using NexaCRM.Services.Admin.Interfaces;
-using NexaCRM.UI.Services;
-using NexaCRM.UI.Services.Interfaces;
-using NexaCRM.UI.Services.Mock;
-using NexaCRM.WebServer.Services;
-using NexaCRM.WebServer;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
-var builder = WebApplication.CreateBuilder(args);
+namespace NexaCRM.WebServer;
 
-builder.WebHost.UseStaticWebAssets();
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
-
-builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
-builder.Services.AddAuthorizationCore();
-
-builder.Services.AddNexaCrmAdminServices();
-
-builder.Services.AddScoped<ActionInterop>();
-builder.Services.AddScoped<IMobileInteractionService, MobileInteractionService>();
-builder.Services.AddScoped<IGlobalActionService, GlobalActionService>();
-builder.Services.AddScoped<INavigationStateService, NavigationStateService>();
-builder.Services.AddScoped<IDeviceService, DeviceService>();
-builder.Services.AddScoped<IUserFavoritesService, UserFavoritesService>();
-builder.Services.AddSingleton<ISyncOrchestrationService, MockSyncOrchestrationService>();
-builder.Services.AddSingleton<ICommunicationHubService, MockCommunicationHubService>();
-builder.Services.AddSingleton<IFileHubService, MockFileHubService>();
-builder.Services.AddSingleton<ISettingsCustomizationService, MockSettingsCustomizationService>();
-builder.Services.AddSingleton<IUserGovernanceService, MockUserGovernanceService>();
-
-builder.Services.AddScoped<IContactService, MockContactService>();
-builder.Services.AddScoped<IDealService, MockDealService>();
-builder.Services.AddScoped<ITaskService, MockTaskService>();
-builder.Services.AddScoped<ISupportTicketService, MockSupportTicketService>();
-builder.Services.AddScoped<IAgentService, MockAgentService>();
-builder.Services.AddScoped<IMarketingCampaignService, MockMarketingCampaignService>();
-builder.Services.AddScoped<IReportService, MockReportService>();
-builder.Services.AddScoped<IActivityService, MockActivityService>();
-builder.Services.AddScoped<ISalesManagementService, MockSalesManagementService>();
-builder.Services.AddScoped<IEmailTemplateService, MockEmailTemplateService>();
-builder.Services.AddScoped<ITeamService, MockTeamService>();
-
-builder.Services.AddScoped<ServerAuthenticationStateProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<ServerAuthenticationStateProvider>());
-builder.Services.AddScoped<IAuthenticationService>(sp => sp.GetRequiredService<ServerAuthenticationStateProvider>());
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+public static class Program
 {
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(NexaCRM.UI.Shared.MainLayout).Assembly);
-
-await StartDuplicateMonitorAsync(app.Services);
-
-app.Run();
-
-static async Task StartDuplicateMonitorAsync(IServiceProvider services)
-{
-    await using var scope = services.CreateAsyncScope();
-    var monitor = scope.ServiceProvider.GetRequiredService<IDuplicateMonitorService>();
-    await monitor.StartAsync();
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
 }
